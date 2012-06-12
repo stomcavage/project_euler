0bc69ababc1a4e071949133dd13bd1d2cbf8d544
stomcavage <stomcavage@weblinc.com>
2012-06-12 13:06:20 -0400

    Solution to project euler problem 3 in Clojure

---
+++ clojure/problem3/project.clj
@@ -0,0 +1,4 @@
+(defproject com/tomcavage/project-euler/problem3 "1.0.0-SNAPSHOT"
+  :description "Solution to Project Euler problem 3"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main com.tomcavage.project-euler.problem3.core)
---
+++ clojure/problem3/test/com/tomcavage/project_euler/problem3/test/core.clj
@@ -0,0 +1,6 @@
+(ns com.tomcavage.project-euler.problem3.test.core
+  (:use [com.tomcavage.project-euler.problem3.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
